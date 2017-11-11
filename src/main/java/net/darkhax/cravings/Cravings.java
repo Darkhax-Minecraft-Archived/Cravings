311b795cea4f5393c51c64def98704481ffeef70
Tyler Hancock <darklime@live.ca>
2017-11-10 23:15:59 -0700

    Added basic configuration.

--- src/main/java/net/darkhax/cravings/Cravings.java
+++ src/main/java/net/darkhax/cravings/Cravings.java
@@ -3,35 +3,48 @@ package net.darkhax.cravings;
 import net.darkhax.bookshelf.lib.LoggingHelper;
 import net.darkhax.bookshelf.lib.WeightedSelector;
 import net.darkhax.cravings.craving.ICraving;
+import net.darkhax.cravings.handler.ConfigurationHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = "cravings", name = "Cravings", version = "@VERSION@", dependencies = "", certificateFingerprint = "@FINGERPRINT@")
 public class Cravings {
-    
+
     /**
      * Logger for the mod. Other mods should not use this!
      */
     public static final LoggingHelper LOG = new LoggingHelper("Cravings");
-    
+
     /**
      * The weighted registry for cravings. All cravings should be registered here. Cravings can
      * be registered at any time, including game/world runtime, however the init phase is the
      * recommended time to do this.
      */
     public static final WeightedSelector<ICraving> CRAVING_REGISTRY = new WeightedSelector<>();
-    
+
+    /**
+     * Configuration instance. Can be used to check fields, but they're usually static.
+     */
+    public static ConfigurationHandler config;
+
+    @EventHandler
+    public void preInit (FMLPreInitializationEvent event) {
+
+        config = new ConfigurationHandler(event.getSuggestedConfigurationFile());
+    }
+
     @EventHandler
     public void init (FMLInitializationEvent event) {
-        
+
         // Register stuff here
     }
-    
+
     @EventHandler
     public void onFingerprintViolation (FMLFingerprintViolationEvent event) {
-        
+
         LOG.warn("Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been tampered with. This version will NOT be supported by the author!");
     }
 }
---
+++ src/main/java/net/darkhax/cravings/handler/ConfigurationHandler.java
@@ -0,0 +1,87 @@
+package net.darkhax.cravings.handler;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+import net.darkhax.cravings.Cravings;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+
+public class ConfigurationHandler {
+
+    public static Configuration config;
+
+    private static final String[] DEFAULT_SATISFIED_EFFECTS = { "minecraft:speed, 60000", "minecraft:haste, 6000", "minecraft:, 6000", "minecraft:regeneration, 100, 2" };
+    private static final String[] DEFUALT_UNSATISFIED_EFFECTS = { "minecraft:slowness", "minecraft:nausea", "minecraft:hunger, 100", "minecraft:weakness, 6000" };
+
+    public static List<PotionEffect> satisfiedEffects = new ArrayList<>();
+    public static List<PotionEffect> unsatisfiedEffects = new ArrayList<>();
+
+    public static boolean applySatisfiedEffects = true;
+    public static boolean applyUnsatisfiedEffects = true;
+
+    public ConfigurationHandler (File file) {
+
+        config = new Configuration(file);
+        this.syncConfigData();
+    }
+
+    private void syncConfigData () {
+
+        satisfiedEffects.clear();
+        unsatisfiedEffects.clear();
+
+        for (final String entry : config.getStringList("cravingSatisfied", Configuration.CATEGORY_GENERAL, DEFAULT_SATISFIED_EFFECTS, "This list contains potion id info for the potion effects that can potentially be given to a player when they satisfy a craving. There are three total arguments, but only the first one is needed. The first argument is the id of the potion effect, this is the string id and not the numeric one! The second argument is the amount of time the player will get the buff for in ticks, default is 200 ticks (10 seconds), the last argument is the amplifier for the potion effect, default is 0. Arguments are split up using a , character and a space.")) {
+
+            satisfiedEffects.add(this.getEffectFromString(entry));
+        }
+
+        for (final String entry : config.getStringList("cravingUnsatisfied", Configuration.CATEGORY_GENERAL, DEFUALT_UNSATISFIED_EFFECTS, "This list contains potion id info for the potion effects that can potentially be given to a player when they fail to satisfy a craving. There are three total arguments, but only the first one is needed. The first argument is the id of the potion effect, this is the string id and not the numeric one! The second argument is the amount of time the player will get the buff for in ticks, default is 200 ticks (10 seconds), the last argument is the amplifier for the potion effect, default is 0. Arguments are split up using a , character and a space.")) {
+
+            unsatisfiedEffects.add(this.getEffectFromString(entry));
+        }
+
+        applySatisfiedEffects = config.getBoolean("applySatisfiedEffects", Configuration.CATEGORY_GENERAL, true, "If enabled, players will get benefits for satisfying cravings.");
+        applyUnsatisfiedEffects = config.getBoolean("applyUnsatisfiedEffects", Configuration.CATEGORY_GENERAL, true, "If enabled, players will get a penalty for not satisfying cravings.");
+
+        if (config.hasChanged()) {
+            config.save();
+        }
+    }
+
+    private PotionEffect getEffectFromString (String effectString) {
+
+        final String[] args = effectString.split(", ");
+
+        // Minimum args check
+        if (args.length == 1) {
+
+            Cravings.LOG.noticableWarning(false, Arrays.asList("The config line " + effectString + " is not valid. At least two arguments are needed. Use , and a space to split args."));
+            return null;
+        }
+
+        // Potion id validation
+        if (!ForgeRegistries.POTIONS.containsKey(new ResourceLocation(args[0]))) {
+
+            Cravings.LOG.noticableWarning(false, Arrays.asList("No potion found for id " + args[0]));
+            return null;
+        }
+
+        final Potion effect = ForgeRegistries.POTIONS.getValue(new ResourceLocation(args[0]));
+
+        // If duration argument is given, try to set it. Otherwise default to 200 (10 seconds)
+        final int duration = args.length == 2 && NumberUtils.isCreatable(args[1]) ? NumberUtils.createInteger(args[1]) : 200;
+
+        // If amplifier arg is given, try to set it. Otherwise default to 0 (level 1)
+        final int amplifier = args.length == 3 && NumberUtils.isCreatable(args[2]) ? NumberUtils.createInteger(args[2]) : 0;
+
+        return new PotionEffect(effect, duration, amplifier);
+    }
+}
