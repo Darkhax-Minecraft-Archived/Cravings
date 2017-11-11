10e973cd2a0107e16aa8e5f75cf26361cc1b24e8
Tyler Hancock <darklime@live.ca>
2017-11-10 21:54:58 -0700

    Set up registry.

--- src/main/java/net/darkhax/cravings/Cravings.java
+++ src/main/java/net/darkhax/cravings/Cravings.java
@@ -1,24 +1,37 @@
 package net.darkhax.cravings;
 
 import net.darkhax.bookshelf.lib.LoggingHelper;
+import net.darkhax.bookshelf.lib.WeightedSelector;
+import net.darkhax.cravings.craving.ICraving;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "cravings", name = "Cravings", version = "@VERSION@", dependencies = "required-after:bookshelf@[2.2.458,);", certificateFingerprint = "@FINGERPRINT@")
+@Mod(modid = "cravings", name = "Cravings", version = "@VERSION@", dependencies = "", certificateFingerprint = "@FINGERPRINT@")
 public class Cravings {
-
+    
+    /**
+     * Logger for the mod. Other mods should not use this!
+     */
     public static final LoggingHelper LOG = new LoggingHelper("Cravings");
-
+    
+    /**
+     * The weighted registry for cravings. All cravings should be registered here. Cravings can
+     * be registered at any time, including game/world runtime, however the init phase is the
+     * recommended time to do this.
+     */
+    public static final WeightedSelector<ICraving> CRAVING_REGISTRY = new WeightedSelector<>();
+    
     @EventHandler
-    public void preInit (FMLPreInitializationEvent event) {
-
+    public void init (FMLInitializationEvent event) {
+        
+        // Register stuff here
     }
-
+    
     @EventHandler
     public void onFingerprintViolation (FMLFingerprintViolationEvent event) {
-
+        
         LOG.warn("Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been tampered with. This version will NOT be supported by the author!");
     }
 }
