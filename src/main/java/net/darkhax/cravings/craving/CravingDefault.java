ca204c6b86db075e2317b8b011e563f770632309
Tyler Hancock <darklime@live.ca>
2017-11-12 14:11:24 -0700

    Added support for forge registry info in the default craving class.

--- src/main/java/net/darkhax/cravings/craving/CravingDefault.java
+++ src/main/java/net/darkhax/cravings/craving/CravingDefault.java
@@ -1,13 +1,14 @@
 package net.darkhax.cravings.craving;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.registries.IForgeRegistryEntry;
 
 /**
  * This default implementation of ICraving provides default logic for handling potion effect
  * craving rewards and punishments. These rewards and punishments are potion based, and pulled
  * from random entries on a config file.
  */
-public abstract class CravingDefault implements ICraving {
+public abstract class CravingDefault extends IForgeRegistryEntry.Impl<ICraving> implements ICraving {
 
     @Override
     public void onCravingSatisfied (EntityPlayer player) {
