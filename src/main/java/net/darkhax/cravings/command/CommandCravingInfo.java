29e7b323ecb9c85a16a9ccd3d586a7968f50db83
Tyler Hancock <darklime@live.ca>
2017-11-12 21:57:05 -0700

    Fixed timer being a bit off in info command.

--- src/main/java/net/darkhax/cravings/command/CommandCravingInfo.java
+++ src/main/java/net/darkhax/cravings/command/CommandCravingInfo.java
@@ -1,7 +1,9 @@
 package net.darkhax.cravings.command;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 import net.darkhax.bookshelf.command.Command;
-import net.darkhax.bookshelf.util.MathsUtils;
 import net.darkhax.cravings.handler.CravingDataHandler;
 import net.darkhax.cravings.handler.CravingDataHandler.ICustomData;
 import net.minecraft.command.CommandException;
@@ -41,15 +43,27 @@ public class CommandCravingInfo extends Command {
             if (data.getCravedItem().isEmpty()) {
 
                 sender.sendMessage(new TextComponentTranslation("commands.cravings.info.empty"));
-                sender.sendMessage(new TextComponentString("Next craving attempt in: " + MathsUtils.ticksToTime(data.getTimeToNextAttempt())));
+                sender.sendMessage(new TextComponentString("Next craving attempt in: " + ticksToTime(data.getTimeToNextAttempt())));
                 return;
             }
 
             else {
 
                 sender.sendMessage(new TextComponentString("You have a craving for " + data.getCravedItem().getDisplayName()));
-                sender.sendMessage(new TextComponentString("Time to satisfy: " + MathsUtils.ticksToTime(data.getTimeToSatisfy())));
+                sender.sendMessage(new TextComponentString("Time to satisfy: " + ticksToTime(data.getTimeToSatisfy())));
             }
         }
+    }
+
+    public static String ticksToTime (int ticks) {
+
+        final SimpleDateFormat format = new SimpleDateFormat("hh:mm:ss", Locale.getDefault());
+
+        final int baseSeconds = ticks / 20;
+        final int seconds = baseSeconds % 60;
+        final int minutes = baseSeconds / 60 % 60;
+        final int hours = baseSeconds / 60 / 60;
+
+        return (hours > 0 ? hours + ":" : "") + minutes + ":" + seconds;
     }
 }
