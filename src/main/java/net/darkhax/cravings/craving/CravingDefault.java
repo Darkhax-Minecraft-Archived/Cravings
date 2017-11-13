008732eeed554ffa7c13d5cd3a808d12645b7119
Tyler Hancock <darklime@live.ca>
2017-11-12 22:15:23 -0700

    Added default random potion entries.

--- src/main/java/net/darkhax/cravings/craving/CravingDefault.java
+++ src/main/java/net/darkhax/cravings/craving/CravingDefault.java
@@ -1,6 +1,8 @@
 package net.darkhax.cravings.craving;
 
+import net.darkhax.cravings.handler.ConfigurationHandler;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.potion.PotionEffect;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 
 /**
@@ -13,10 +15,14 @@ public abstract class CravingDefault extends IForgeRegistryEntry.Impl<ICraving> 
     @Override
     public void onCravingSatisfied (EntityPlayer player) {
 
+        final PotionEffect effect = ConfigurationHandler.satisfiedEffects.get(player.getRNG().nextInt(ConfigurationHandler.satisfiedEffects.size()));
+        player.addPotionEffect(new PotionEffect(effect));
     }
 
     @Override
     public void onCravingUnsatisifed (EntityPlayer player) {
 
+        final PotionEffect effect = ConfigurationHandler.unsatisfiedEffects.get(player.getRNG().nextInt(ConfigurationHandler.unsatisfiedEffects.size()));
+        player.addPotionEffect(new PotionEffect(effect));
     }
 }
