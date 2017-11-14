011d7b7eaab7f0ee7b367a1d283b5a97001644e4
Tyler Hancock <darklime@live.ca>
2017-11-14 15:15:10 -0700

    Added some protection against null potion effects.

--- src/main/java/net/darkhax/cravings/craving/CravingDefault.java
+++ src/main/java/net/darkhax/cravings/craving/CravingDefault.java
@@ -1,7 +1,9 @@
 package net.darkhax.cravings.craving;
 
+import net.darkhax.cravings.Cravings;
 import net.darkhax.cravings.handler.ConfigurationHandler;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
 import net.minecraft.potion.PotionEffect;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 
@@ -16,13 +18,24 @@ public abstract class CravingDefault extends IForgeRegistryEntry.Impl<ICraving> 
     public void onCravingSatisfied (EntityPlayer player) {
 
         final PotionEffect effect = ConfigurationHandler.satisfiedEffects.get(player.getRNG().nextInt(ConfigurationHandler.satisfiedEffects.size()));
-        player.addPotionEffect(new PotionEffect(effect));
+        player.addPotionEffect(this.copyPotion(effect));
     }
 
     @Override
     public void onCravingUnsatisifed (EntityPlayer player) {
 
         final PotionEffect effect = ConfigurationHandler.unsatisfiedEffects.get(player.getRNG().nextInt(ConfigurationHandler.unsatisfiedEffects.size()));
-        player.addPotionEffect(new PotionEffect(effect));
+        player.addPotionEffect(this.copyPotion(effect));
+    }
+
+    private PotionEffect copyPotion (PotionEffect pot) {
+
+        if (pot == null || pot.getPotion() == null) {
+
+            Cravings.LOG.error("Attempted to give the player a null craving reward! The mod is likely configured wrongly.");
+            return new PotionEffect(MobEffects.SPEED, 0, 0, false, false);
+        }
+
+        return new PotionEffect(pot.getPotion(), pot.getDuration(), pot.getAmplifier(), false, false);
     }
 }
